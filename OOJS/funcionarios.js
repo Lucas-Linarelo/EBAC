4404a7d0c391629ecb478114d61118cf0c305e1d
Lucas Linarelo <linarelolcs@gmail.com>
2025-02-18 18:06:41 -0300

    1.0

---
+++ OOJS/funcionarios.js
@@ -0,0 +1,52 @@
+function Pessoa(nome){
+    this.nome=nome;
+    this.dizOi = function(){
+        console.log(this.nome+" diz olá.");
+    }
+}
+
+function Funcionario(nome, cargo,salario){
+    this.cargo=cargo;
+    this.dizCargo = function(){
+        console.log (`${nome} trabalha como ${cargo},`);
+    }
+
+// Getters&Setters
+//Encapsulamento
+    let _salario = salario;
+    this.getSalario = function(){
+        return _salario;
+    }
+//simulação promoção + validação
+    this.setSalario=function(valor){
+        if(typeof valor ==='number'){
+            _salario=valor;
+        }
+    }
+
+    this.aumento=function(){
+        const novoSalario = _salario*1.1;
+        _salario=novoSalario;
+    }
+
+
+    Pessoa.call(this, nome);
+}
+
+function Estagiario(nome){
+    Funcionario.call(this,nome, "Estágiario", 2000)
+
+    this.aumento=function(){
+        const novoSalario = this.getSalario()*1.07;
+        this.setSalario(novoSalario);
+    }
+
+}
+
+const funcionario1 = new Funcionario("Maria", "desenvolvedora Front-end", 5000);
+const funcionario2=new Estagiario("Pedro");
+
+funcionario1.aumento();
+console.log(funcionario1.getSalario())
+funcionario2.aumento();
+console.log(funcionario2.getSalario())
